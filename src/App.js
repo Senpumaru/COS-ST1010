78e2da47e63e78564e42a169e2d52e11f8bce5df
Senpumaru <hakumen737@gmail.com>
2021-07-08 11:11:44 +0300

    Git Commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Toolbar from "@material-ui/core/Toolbar";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Header from './components/Menu/Header/AppBar';
+import CaseUpdateForm from './components/Cases/Forms/UpdateForm';
+
+import theme from "./components/Theme/Theme";
+import ErrorScreen from './screens/Account/ErrorScreen';
+import LoginScreen from './screens/Account/LoginScreen';
+import Reg from './screens/Account/Reg';
+import Dashboard from './screens/Dashboard.js';
+import CaseBoard from './screens/Cases/CaseBoard';
+import CaseArchive from './screens/Cases/CaseArchive';
+import CaseReview from './screens/Cases/CaseReview';
+
+
+
+/*** Material UI Styles Override ***/
+const useStyles = makeStyles(theme => ({
+  app: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1)
+  }
+}));
+
+function AppProtectedRoute({ application: App, component: Component, ...rest }) {
+
+  const userLogin = useSelector(state => state.Profile["userLogin"])
+  const { loading, error, userInfo } = userLogin
+
+  return (
+    <Route {...rest} render={
+      (props) => {
+        if (userInfo != null) {
+
+          switch (App) {
+            case "ST1010":
+              if (userInfo["application_rights"]["ST1010"] === true) {
+                return <Component {...props} />
+
+              } else {
+                return <Redirect to="/Error" />
+              }
+            
+            default:
+              return <Component {...props} />
+          }
+        } else {
+          return <Redirect to="/Login" />
+        }
+      }
+    } />
+  )
+}
+
+function App() {
+  /*** Material UI Styles ***/
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          
+          <Route path='/Register' component={Reg} />
+          <Route path="/Login" component={LoginScreen} />
+          <Route path="/Error" component={ErrorScreen} />
+          {/* URL Error screen for invalid URL  */}
+          
+          <React.Fragment>
+            <div className={classes.app}>
+              <Header />
+              <main className={classes.content}>
+                <Toolbar />
+                <AppProtectedRoute exact path="/" component={Dashboard} />
+                <AppProtectedRoute exact path="/ST1010" application="ST1010" component={CaseBoard} />
+                <AppProtectedRoute exact path="/ST1010/Case/:id" application="ST1010" component={CaseUpdateForm} />
+                <AppProtectedRoute exact path="/ST1010/Case/Review/:id" application="ST1010" component={CaseReview} />
+                <AppProtectedRoute exact path="/ST1010/Case/:code/:number/Archive" application="ST1010" component={CaseArchive} />
+                
+              </main>
+            </div>
+          </React.Fragment>
+          <Route render={() => <Redirect to={{pathname: "/Error"}} />} /> 
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+
+export default App;
---
+++ src/Store.js
@@ -0,0 +1,59 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk"
+import { userLoginReducer, userRegisterReducer } from './reducers/Accounts/UserReducers'
+import { approvalUpdateReducer, caseCreateReducer, caseDetailsReducer, caseListReducer, caseStatisticsReducer, caseTransferReducer, caseUpdateReducer } from './reducers/Cases/CaseReducers'
+import { searchReducerALK } from "./reducers/Cases/SearchReducers"
+
+
+
+
+/*** All reducers ***/
+const reducer = combineReducers({
+    Profile: combineReducers({
+        userRegister: userRegisterReducer,
+        userLogin: userLoginReducer,
+    }),
+    ST1010: combineReducers({
+        caseCreate: caseCreateReducer,
+        caseDetails: caseDetailsReducer,
+        caseUpdate: caseUpdateReducer,
+        caseTransfer: caseTransferReducer,
+        caseList: caseListReducer,
+        caseStatistics: caseStatisticsReducer,
+        approvalUpdate: approvalUpdateReducer,
+        searchParameters: searchReducerALK,
+    }),
+})
+
+/* Profile Storage */
+const userInfoFromStorage = sessionStorage.getItem('userInfo') ?
+    JSON.parse(sessionStorage.getItem('userInfo')) : null
+
+const initialState = {
+    Profile: {
+        userLogin: {
+            userInfo: userInfoFromStorage
+        }
+    },
+    ST1010: {
+        caseStatistics: {
+            caseStats: {
+                cases_count: 0,
+                cases_in_work: 0,
+                cases_done: 0,
+            }
+        }
+    }
+}
+
+/*** Middleware ***/
+// Collect all middleware
+const middleware = [thunk]
+/* Create Store */
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store;
